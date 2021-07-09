61fb4f5ca1b38a59c779f590f6723cfa7bcf3cda
a503424551 <503424551@qq.com>
2021-07-09 09:52:47 +0800

    一套勾和线

--- app/src/main/java/com/yan/demo/view/ProductProgress.java
+++ app/src/main/java/com/yan/demo/view/ProductProgress.java
@@ -17,7 +17,7 @@ public class ProductProgress extends View {
     private int gouLengForFirstPointX;
     private int gouLengForFirstPointY;
     private int gouSecondPointXOffset;
-    private int gouThirdPointY;
+    private int gouSecondPointYOffset;
     private int gouLengForSecondPointXOffset;
 
     public ProductProgress(Context context) {
@@ -38,19 +38,21 @@ public class ProductProgress extends View {
         gouFirstPointX = 30;
         //勾第一笔左上边点Y轴坐标
         gouFirstPointY = 70;
-        //√底部角度交叉点X轴距离左上边x坐标的x轴距离
+        //√底部角度交叉点X轴距离 勾第一笔左上边点x轴坐标的x轴距离
         gouLengForFirstPointX = 20;
-        //√底部角度交叉点Y轴距离左上边Y坐标的Y轴距离
+        //√底部角度交叉点Y轴距离勾第一笔左上边点Y坐标的Y轴距离
         gouLengForFirstPointY = 20;
-        //√第二笔左下边距离第一笔右下边的距离
-        gouSecondPointXOffset = -5;
+        //√第二笔左下边X轴距离第一笔右下边X轴的距离
+        gouSecondPointXOffset = 5;
         //勾第二笔右上边点Y轴相对第一笔左上边点Y轴的距离
-        gouThirdPointY = -30;
-        //勾第二笔左上边点X轴距离底部角度交叉点X轴的距离
+        gouSecondPointYOffset = 30;
+        //勾第二笔右上边点X轴距离底部角度交叉点X轴的距离
         gouLengForSecondPointXOffset = 50;
         canvas.drawLine(gouFirstPointX, gouFirstPointY, gouFirstPointX + gouLengForFirstPointX, gouLengForFirstPointY + gouFirstPointY, paint);
-    canvas.drawLine(gouFirstPointX + gouLengForFirstPointX +gouSecondPointXOffset, gouFirstPointY + gouLengForFirstPointY, gouFirstPointX + gouLengForFirstPointX + gouLengForSecondPointXOffset, gouFirstPointY+gouThirdPointY, paint);
-        //   canvas.drawLine(gouFirstPointX+gouLengForX+gouFirstPointY+gouLengForY,65,30+50-30+90-45+120,65,paint);
-        //  canvas.drawLine(30+50-30+90-45,65,30+50-30+90-45+120,65,paint);
+    canvas.drawLine(gouFirstPointX + gouLengForFirstPointX -gouSecondPointXOffset, gouFirstPointY + gouLengForFirstPointY,
+            gouFirstPointX + gouLengForFirstPointX + gouLengForSecondPointXOffset, gouFirstPointY- gouSecondPointYOffset, paint);
+        canvas.drawLine(gouFirstPointX+gouLengForFirstPointX+gouLengForSecondPointXOffset,(gouFirstPointY+gouLengForFirstPointY)/2+ gouSecondPointYOffset,
+                (gouFirstPointX+gouLengForFirstPointX+gouLengForSecondPointXOffset)+160,(gouFirstPointY+gouLengForFirstPointY)/2+ gouSecondPointYOffset,paint);
+
     }
 }
